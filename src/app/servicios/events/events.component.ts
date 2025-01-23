7e80ee26347e60e568caef4193c65e6f88446223
JulenZZZ <julenz485@gmail.com>
2025-01-23 21:08:07 +0100

    adapted texts and images

--- src/app/servicios/events/events.component.ts
+++ src/app/servicios/events/events.component.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 import { NavbarComponent } from "../../navbar/navbar.component";
 import { RouterModule, RouterOutlet } from "@angular/router";
+import { TranslateModule } from "@ngx-translate/core";
 
 
 @Component({
     selector: 'events-component',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, TranslateModule],
     templateUrl: './events.component.html'
 
   })
